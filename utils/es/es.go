291374f51d4d75b8b79b28a2f06a5f56af39b731
njcx <njcx86@gmail.com>
2019-11-04 18:27:34 +0800

    n,

--- utils/es/es.go
+++ utils/es/es.go
@@ -1,132 +1,138 @@
 package es
 
+
 import (
-	"encoding/json"
+	"context"
 	"fmt"
-	"github.com/olivere/elastic"
-	"reflect"
-	"testing"
+	"k8s.io/klog"
 	"time"
+
+	elastic7 "github.com/olivere/elastic/v7"
+	"github.com/pborman/uuid"
 )
 
-type Tweet struct {
-	User     string                `json:"user"`
-	Age      int                   `json:"age"`
-	Message  string                `json:"message"`
-	Retweets int                   `json:"retweets"`
-	Image    string                `json:"image,omitempty"`
-	Created  time.Time             `json:"created,omitempty"`
-	Tags     []string              `json:"tags,omitempty"`
-	Location string                `json:"location,omitempty"`
-	Suggest  *elastic.SuggestField `json:"suggest_field,omitempty"`
+type Elastic7Wrapper struct {
+	client        *elastic7.Client
+	pipeline      string
+	bulkProcessor *elastic7.BulkProcessor
 }
 
-var mapping = `{
-	"settings":{
-		"number_of_shards": 3,
-		"number_of_replicas": 1
-	},
-	"mappings":{
-		"doc":{
-			"properties":{
-				"user":{
-					"type":"keyword"
-				},
-				"age":{
-					"type": "integer"
-				},
-				"message":{
-					"type":"text",
-					"store": true,
-					"fielddata": true
-				},
-				"image":{
-					"type":"keyword"
-				},
-				"created":{
-					"type":"date"
-				},
-				"tags":{
-					"type":"keyword"
-				},
-				"location":{
-					"type":"geo_point"
-				},
-				"suggest_field":{
-					"type":"completion"
+func NewEsClient7(config ElasticConfig, bulkWorkers int, pipeline string) (*Elastic7Wrapper, error) {
+	var startupFns []elastic7.ClientOptionFunc
+
+	if len(config.Url) > 0 {
+		startupFns = append(startupFns, elastic7.SetURL(config.Url...))
+	}
+
+	if config.User != "" && config.Secret != "" {
+		startupFns = append(startupFns, elastic7.SetBasicAuth(config.User, config.Secret))
+	}
+
+	if config.MaxRetries != nil {
+		startupFns = append(startupFns, elastic7.SetMaxRetries(*config.MaxRetries))
+	}
+
+	if config.HealthCheck != nil {
+		startupFns = append(startupFns, elastic7.SetHealthcheck(*config.HealthCheck))
+	}
+
+	if config.HealthCheck != nil {
+		startupFns = append(startupFns, elastic7.SetHealthcheck(*config.HealthCheck))
+	}
+
+	if config.Timeout != nil {
+		startupFns = append(startupFns, elastic7.SetHealthcheckTimeoutStartup(*config.Timeout))
+	}
+
+	if config.HttpClient != nil {
+		startupFns = append(startupFns, elastic7.SetHttpClient(config.HttpClient))
+	}
+
+	if config.Sniff != nil {
+		startupFns = append(startupFns, elastic7.SetSniff(*config.Sniff))
+	}
+
+	client, err := elastic7.NewClient(startupFns...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to an ElasticSearch Client: %v", err)
+	}
+	bps, err := client.BulkProcessor().
+		Name("ElasticSearchWorker").
+		Workers(bulkWorkers).
+		After(bulkAfterCBV7).
+		BulkActions(1000).               // commit if # requests >= 1000
+		BulkSize(2 << 20).               // commit if size of requests >= 2 MB
+		FlushInterval(10 * time.Second). // commit every 10s
+		Do(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("failed to an ElasticSearch Bulk Processor: %v", err)
+	}
+
+	return &Elastic7Wrapper{client: client, bulkProcessor: bps, pipeline: pipeline}, nil
+}
+
+func (es *Elastic7Wrapper) IndexExists(indices ...string) (bool, error) {
+	return es.client.IndexExists(indices...).Do(context.Background())
+}
+
+func (es *Elastic7Wrapper) CreateIndex(name string, mapping string) (bool, error) {
+	res, err := es.client.CreateIndex(name).Do(context.Background())
+	if err != nil {
+		return false, err
+	}
+	return res.Acknowledged, err
+}
+
+func (es *Elastic7Wrapper) getAliases(index string) (*elastic7.AliasesResult, error) {
+	return es.client.Aliases().Index(index).Do(context.Background())
+}
+
+func (es *Elastic7Wrapper) AddAlias(index string, alias string) (bool, error) {
+	res, err := es.client.Alias().Add(index, alias).Do(context.Background())
+	if err != nil {
+		return false, err
+	}
+	return res.Acknowledged, err
+}
+
+func (es *Elastic7Wrapper) HasAlias(indexName string, aliasName string) (bool, error) {
+	aliases, err := es.getAliases(indexName)
+	if err != nil {
+		return false, err
+	}
+	return aliases.Indices[indexName].HasAlias(aliasName), nil
+}
+
+func (es *Elastic7Wrapper) AddBulkReq(index, typeName string, data interface{}) error {
+	req := elastic7.NewBulkIndexRequest().
+		Index(index).
+		Type(typeName).
+		Id(uuid.NewUUID().String()).
+		Doc(data)
+	if es.pipeline != "" {
+		req.Pipeline(es.pipeline)
+	}
+
+	es.bulkProcessor.Add(req)
+	return nil
+}
+
+func (es *Elastic7Wrapper) FlushBulk() error {
+	return es.bulkProcessor.Flush()
+}
+
+func bulkAfterCBV7(_ int64, _ []elastic7.BulkableRequest, response *elastic7.BulkResponse, err error) {
+	if err != nil {
+		klog.Warningf("Failed to execute bulk operation to ElasticSearch: %v", err)
+	}
+
+	if response.Errors {
+		for _, list := range response.Items {
+			for name, itm := range list {
+				if itm.Error != nil {
+					klog.V(3).Infof("Failed to execute bulk operation to ElasticSearch on %s: %v", name, itm.Error)
 				}
 			}
 		}
 	}
-}`
-
-func TestPingNode(t *testing.T) {
-	PingNode()
-}
-
-func TestIndexExists(t *testing.T) {
-	result := IndexExists("car_source", "test")
-	fmt.Println("all index exists: ", result)
-}
-
-func TestDeleteIndex(t *testing.T) {
-	result := DelIndex("twitter")
-	fmt.Println("all index deleted: ", result)
-}
-
-func TestCreateIndex(t *testing.T) {
-	result := CreateIndex("twitter", mapping)
-	fmt.Println("mapping created: ", result)
-}
-
-func TestBatch(t *testing.T) {
-	tweet1 := Tweet{User: "Jame1", Age: 23, Message: "Take One", Retweets: 1, Created: time.Now()}
-	tweet2 := Tweet{User: "Jame2", Age: 32, Message: "Take Two", Retweets: 0, Created: time.Now()}
-	tweet3 := Tweet{User: "Jame3", Age: 32, Message: "Take Three", Retweets: 0, Created: time.Now()}
-	Batch("twitter", "doc", tweet1, tweet2, tweet3)
-}
-
-func TestGetDoc(t *testing.T) {
-	var tweet Tweet
-	data := GetDoc("twitter", "1")
-	if err := json.Unmarshal(data, &tweet); err == nil {
-		fmt.Printf("data: %v\n", tweet)
-	}
-}
-
-func TestTermQuery(t *testing.T) {
-	var tweet Tweet
-	result := TermQuery("twitter", "doc", "user", "Take Two")
-	//获得数据, 方法一
-	for _, item := range result.Each(reflect.TypeOf(tweet)) {
-		if t, ok := item.(Tweet); ok {
-			fmt.Printf("tweet : %v\n", t)
-		}
-	}
-	//获得数据, 方法二
-	fmt.Println("num of raws: ", result.Hits.TotalHits)
-	if result.Hits.TotalHits > 0 {
-		for _, hit := range result.Hits.Hits {
-			err := json.Unmarshal(*hit.Source, &tweet)
-			if err != nil {
-				fmt.Printf("source convert json failed, err: %v\n", err)
-			}
-			fmt.Printf("data: %v\n", tweet)
-		}
-	}
-}
-
-func TestSearch(t *testing.T) {
-	result := Search("twitter", "doc")
-	var tweet Tweet
-	for _, item := range result.Each(reflect.TypeOf(tweet)) {
-		if t, ok := item.(Tweet); ok {
-			fmt.Printf("tweet : %v\n", t)
-		}
-	}
-}
-
-func TestAggsSearch(t *testing.T) {
-	AggsSearch("twitter", "doc")
-
 }
