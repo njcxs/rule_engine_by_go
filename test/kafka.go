c24678a4efdcb1049081e5106e57d40ac21fa886
njcx <njcx86@gmail.com>
2020-10-28 16:41:52 +0800

    Default Changelist

--- test/kafka.go
+++ test/kafka.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 
-	kafkaConsumer := kafka.InitKakfaConsumer([]string{"10.10.128.235:9093"}, "test", []string{"hids"})
+	//kafkaConsumer := kafka.InitKakfaConsumer([]string{"10.10.128.235:9093"}, "test", []string{"hids"})
+
+	kafkaConsumer := kafka.InitKakfaConsumer([]string{"172.21.129.2:9092"}, "test", []string{"nids-conn"})
 	kafkaConsumer.Open()
 
 	for {
