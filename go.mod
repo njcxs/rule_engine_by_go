e5319970391776ad6e6596c9d85765770f59c968
njcx <njcx86@gmail.com>
2021-02-24 16:56:22 +0800

    Default Changelist

--- go.mod
+++ go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.4.2
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/json-iterator/go v1.1.10
 	github.com/segmentio/kafka-go v0.4.5 // indirect
+	github.com/tidwall/match v1.0.3
+	github.com/tidwall/pretty v1.1.0
 	gopkg.in/yaml.v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
+//package main
+//
+//import (
+//	"fmt"
+//	"rule_engine_by_go/utils"
+//	"rule_engine_by_go/utils/log"
+//)
+//
+//func main() {
+//
+//	config, _ := utils.GetConfig()
+//	log.Info.Println("test")
+//	fmt.Println(config["input"])
+//}
+
 package main
 
-import (
-	"fmt"
-	"rule_engine_by_go/utils"
-	"rule_engine_by_go/utils/log"
-)
+import "rule_engine_by_go/utils/json"
+
+const json_ = `{"name":{"first":"Janet","last":"Prichard"},".age":47}`
 
 func main() {
-
-	config, _ := utils.GetConfig()
-	log.Info.Println("test")
-	fmt.Println(config["input"])
+	value := json.Get(json_, "name.last")
+	println(value.String())
 }
